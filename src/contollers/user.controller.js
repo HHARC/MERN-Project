4a870b4b4b45d0babe0595b1f5ba4ff89e15693e
Hamza Abdul Rauf <hharc123@gmail.com>
2025-05-04 02:24:33 -0700

    Video 21

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -9,6 +9,7 @@ import { uploadOnCloudinary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens = async (userId) => {
   try {
@@ -367,9 +368,59 @@ status(200)
 
 })
 
+const getUserHistory=asyncHandler(async(req,res)=>{
+
+  const user= await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      },
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            },
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner",
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+   
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "watchHistory fetch Successfully"
+    )
+  )
+})
 
 export {
   registerUser, loginUser, logoutUser, refreshTokenAccess,
   changeCurrentPassword, getCurrentUser, updateAccountDetails,
-  updateUserAvatar, updateUserCoverImage,getUserChannelProfile
+  updateUserAvatar, updateUserCoverImage,getUserChannelProfile,getUserHistory
 };
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -41,6 +41,7 @@ const videoSchema= new Schema({
     timestamps:true,
 }
 )
+
 videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video=mongoose.model("Video", videoSchema)
